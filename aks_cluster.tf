4023fc3a5c255e2ce20a1975e4950bc7925971af
KennedyUC <chukwukakennedyuche@gmail.com>
2022-01-19 08:47:43 -0800

    terraform files modified

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -49,3 +49,4 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.resource-group.name
   virtual_network_name = azurerm_virtual_network.azure-vnet.name
   address_prefix       = "10.30.1.0/24"
+}
