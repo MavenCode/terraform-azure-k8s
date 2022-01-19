1c2ff61ff229a8633a29df41cf981ba86deac8d5
KennedyUC <chukwukakennedyuche@gmail.com>
2022-01-19 05:23:29 -0800

    terraform files updated

--- provider.tf
+++ provider.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "azurerm" {
     storage_account_name = var.storage_account_name
-    container_name       = var.container_name
-    key                  = var.key
     subscription_id      = var.subscription_id
     tenant_id            = var.tenant_id
 
--- var.tf
+++ var.tf
@@ -7,12 +7,6 @@ variable "serviceprinciple_key" {
 variable "storage_account_name" {
 }
 
-variable "container_name" {
-}
-
-variable "key" {
-}
-
 variable "subscription_id" {
 }
 
