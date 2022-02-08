344b8d01f6f01f16403eab20b9f259c85bd0132a
Geewynn <ekainugodwin@gmail.com>
2022-02-08 22:56:37 +0100

    github action setup

--- example/main.tf
+++ example/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "k8s_setup" {
-  source = "../"
+  source = "https://github.com/MavenCode/terraform-azure-k8s.git"
 
   cluster_name            = var.cluster_name
   resource_group_name     = azurerm_resource_group.rg.name
