b22ecc64b00d1e7443b1ad7e070c72f95899d454
charles adetiloye <charlesa101@users.noreply.github.com>
2023-11-21 15:58:56 -0600

    adding private ip config vars

--- variables.tf
+++ variables.tf
@@ -11,11 +11,23 @@ variable "node_pool_osdisk_size" {}
 variable "node_pool_max_count" {}
 variable "node_pool_min_count" {}
 variable "network_plugin" {}
-variable "is_internal_lb" {}
 variable "dns_prefix" {}
 
 variable "client_id" {}
 variable "client_secret" {}
+
+variable "api_server_authorized_ip_ranges" {
+  description = "Ip ranges allowed to interact with Kubernetes API. Default no restrictions"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_cluster_enabled" {
+  description = "Configure AKS as a Private Cluster: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/kubernetes_cluster#private_cluster_enabled"
+  type        = bool
+  default     = true
+}
+
 
 variable "env" {}
 
